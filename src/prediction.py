6cade62b7d72a4abde1d2127a6bcbf7ee4407c6d
BHARATHSEESA <23eg110e25@anurag.edu.in>
2026-03-11 14:36:32 +0530

    fixed artifact path for streamlit

--- src/prediction.py
+++ src/prediction.py
@@ -6,14 +6,22 @@
 
 import pickle
 import numpy as np
+import os
 
 class Insurance_Prediction:
 
-    def __init__(self):   # ✅ correct constructor
-        with open("artifacts/scaler.pkl", "rb") as f:
+    def __init__(self):
+
+        # Get project root directory
+        base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+        scaler_path = os.path.join(base_dir, "artifacts", "scaler.pkl")
+        model_path = os.path.join(base_dir, "artifacts", "model.pkl")
+
+        with open(scaler_path, "rb") as f:
             self.scaler = pickle.load(f)
 
-        with open("artifacts/model.pkl", "rb") as f:
+        with open(model_path, "rb") as f:
             self.model = pickle.load(f)
 
     def prediction(self, Age, Annual_Income_LPA, Policy_Term_Years, Sum_Assured_Lakhs):
