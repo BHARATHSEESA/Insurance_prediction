2b6ef621f021b8d0ca2033e318b5e50d38c6fb79
BHARATHSEESA <23eg110e25@anurag.edu.in>
2026-03-11 13:52:26 +0530

    initial commit

---
+++ src/prediction.py
@@ -0,0 +1,20 @@
+# 1. Load scaler.pkl and model.pkl
+# 2. Get the inputs from the user
+# 3. Scale the inputs
+# 4. Predict the output
+# 5. Print the output
+
+import pickle
+import numpy as np
+
+class Insurance_Prediction:
+    def _init_(self):
+        with open("../artifacts/scaler.pkl","rb") as f:
+            self.scaler=pickle.load(f)
+        with open("../artifacts/model.pkl","rb") as f:
+            self.model=pickle.load(f)
+    def prediction(self,Age,Annual_Income_LPA,Policy_Term_Years,Sum_Assured_Lakhs):
+        input_data = np.array([[Age, Annual_Income_LPA, Policy_Term_Years, Sum_Assured_Lakhs]])
+        scaled_input = self.scaler.transform(input_data)
+        result = self.model.predict(scaled_input)
+        return result[0]
