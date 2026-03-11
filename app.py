dc50a01dd38ee7a58890f6fb2eca1fcbc98e3a59
BHARATHSEESA <23eg110e25@anurag.edu.in>
2026-03-11 14:24:25 +0530

    initial commit

--- app.py
+++ app.py
@@ -3,13 +3,15 @@ from src.prediction import Insurance_Prediction
 
 st.title("Insurance Premium Prediction")
 
-Age = st.number_input("Enter Age")
+Age = st.number_input("Age")
 Annual_Income_LPA = st.number_input("Annual Income (LPA)")
-Policy_Term_Years = st.number_input("Policy Term (Years)")
+Policy_Term_Years = st.number_input("Policy Term Years")
 Sum_Assured_Lakhs = st.number_input("Sum Assured (Lakhs)")
 
-if st.button("Predict Premium"):
+if st.button("Predict"):
+
     model = Insurance_Prediction()
+
     result = model.prediction(Age, Annual_Income_LPA, Policy_Term_Years, Sum_Assured_Lakhs)
 
     st.success(f"Predicted Premium: {result}")
